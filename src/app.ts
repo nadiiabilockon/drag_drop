1d00ebba77635b6ae3014af28ef0bb537dad04a5
nadiiabilockon <11071995nad@gmail.com>
2020-03-23 15:38:28 +0200

    added input validation

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,39 @@
+//Validation
+interface Validatable {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+}
+
+function validate(validatableInput: Validatable) {
+    let isValid = true;
+    if (validatableInput.required) {
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0
+    }
+    if (validatableInput.minLength != null && typeof validatableInput.value === 'string') {
+        isValid = isValid && validatableInput.value.length > validatableInput.minLength;
+    }
+    if (validatableInput.maxLength != null && typeof validatableInput.value === 'string') {
+        isValid = isValid && validatableInput.value.length < validatableInput.maxLength;
+    }
+    if (validatableInput.max != null && typeof validatableInput.value === 'number') {
+        isValid = isValid && validatableInput.value <= validatableInput.max
+    }
+    if (validatableInput.min != null && typeof validatableInput.value === 'number') {
+        isValid = isValid && validatableInput.value >= validatableInput.min
+    }
+    return isValid
+}
+
 //autobind decorator 
 function autobind(_target: any, _methodName: string, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;
     const adjDescriptor: PropertyDescriptor = {
         configurable: true,
-        get () {
+        get() {
             const boundFn = originalMethod.bind(this);
             return boundFn
         }
@@ -11,6 +41,23 @@ function autobind(_target: any, _methodName: string, descriptor: PropertyDescrip
     return adjDescriptor
 }
 
+//ProjectList Class
+class ProjectList {
+    templeteElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLElement;
+
+    constructor(private type){
+        this.templeteElement = document.getElementById('project-list')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const importedNode = document.importNode(this.templeteElement.content, true)
+        this.element = importedNode.firstElementChild as HTMLElement;
+        this.element.id = ''
+    }
+}
+
+//ProjectInput Class
 class ProjectInput {
     templeteElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -36,10 +83,51 @@ class ProjectInput {
         this.attach()
     }
 
+    private getUserInput(): [string, string, number] | void {
+        const enterdTitle = this.titleInputElement.value;
+        const enterdDescription = this.descriptionInputElement.value;
+        const enterdPeople = this.peopleInputElement.value;
+
+        const titleValidatable: Validatable = {
+            value: enterdTitle,
+            required: true
+        }
+        const descriptionValidatable: Validatable = {
+            value: enterdDescription,
+            required: true,
+            minLength: 5
+        }
+        const peopleValidatable: Validatable = {
+            value: +enterdPeople,
+            required: true,
+            min: 1,
+            max: 5
+        }
+
+        if (!validate(titleValidatable) || !validate(descriptionValidatable) || !validate(peopleValidatable)) {
+            alert('Ivalid input, please try again!')
+            return
+        } else {
+            return [enterdTitle, enterdDescription, +enterdPeople]
+        }
+    }
+
+    private clearInput() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
+
     @autobind
     private submitHandler(event: Event) {
         event.preventDefault();
-        console.log(this.titleInputElement.value)
+        const userInput = this.getUserInput();
+
+        if (Array.isArray(userInput)) {
+            const [title, description, people] = userInput;
+            console.log(title, description, people);
+            this.clearInput()
+        }
     }
 
     private configure() {
